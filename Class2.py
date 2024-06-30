c1136e6ebd8d467a80dde4731e7cb18cedd9bc43
Paras Kataria <161503421+ParasKataria27@users.noreply.github.com>
2024-07-01 00:06:53 +0530

    Class 2 of Python

---
+++ Class2.py
@@ -0,0 +1,94 @@
+#Variable :- It is a short name of any values
+#or
+#Variable are used to store the data
+#or
+#variable is a container where we store the values.
+#x= 'india' 
+#print(x)
+
+#x is variable and india is value
+
+
+# Types of variable
+# 1. Single Variable
+#x = 45
+#y = 60
+
+#print(x)
+#print(y)
+
+
+# 2. Multiple Variable
+
+#x,y = 45,60
+
+"""
+Comments :- It is Statement we want to show but dont want to execute in
+python code
+
+Types of comments
+
+1. Single Comments :- #
+2. Multiple Comments :- '''  ''' or """
+
+'''x = 1500
+y = 2000
+print(x,y)
+'''
+#----------------
+
+'''
+Limitation of Variables
+1. Variable should be start with alphabets
+ Ex. A1 = Right
+     1A = Wrong
+
+2. Variable Can be alphanumeric
+ Ex. X2,Y3,Z3  = right
+     2X,3Y,3Z = Wrong
+
+3. Variable is case sensitive
+4. Here You cannot insert any special character ($%^&*)
+5. Here You Can Use (_) Under_Score in Variable '''
+
+'''A = "Hello World"
+print(A)
+
+b2 = 455
+print(b2)
+
+Name = "akaaltheBAnd"
+print(Name)
+'''
+# Indentation Error:-
+
+ #print("Hello world")
+
+ #-- addition Subtraction Multiplication Division
+
+x = 45
+y = 50
+
+print(x+y)
+print("x=45,y=50")
+print("addition :-",x+y)
+print ("Subtract",x-y)
+print("multiply",x*y)
+print("divide",x/y)
+
+#Print the sum of three number that given below
+x = 45
+y = 60
+z = 80
+
+print(x+y+z)
+
+#Print the multiplication of four number
+a,b,c,d = 5,4,8,9
+
+print(a*b*c*d)
+
+#Print your name in With variable
+P = "Paras Kataria"
+
+print(P)
